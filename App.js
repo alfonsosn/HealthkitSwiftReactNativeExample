45813124d3f5808bb440bcefc9bceccacd603cfa
alfonsosn <alfonso.en.ca@gmail.com>
2020-10-23 21:43:59 -0400

    INTEGRATION

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HealthkitController from './Healthkit.js';
 
 export default function App() {
+  React.useEffect(() => {
+    HealthkitController.requestAuthorization();
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
